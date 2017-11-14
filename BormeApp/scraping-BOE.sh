bce9154a7093c352241d012687a1986579892239
fantalavapies <fanta@mierda.tv>
2017-11-14 21:56:03 +0100

    script bash scraping boe

---
+++ BormeApp/scraping-BOE.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script para automatizar el scrapeo de datos de BOE, BORME, BOCM. Concretamente para BOE
+# A mano se lanza así:
+# node app BOE 2001
+# node app BORME 2009
+# node app BOCM 2010
+# Es decir, se lanza indicando BOE/BORME/BOCM y luego el AÑO.
+# Este script es para no tener que andar lanzando a mano. Lo lanzará todo del tirón.
+# bash scraping-BOE
+
+anoInicioBOE=2001
+anoInicioBORME=2009
+anoInicioBOCM=2010
+anoActual=$(date +"%Y")
+mkdir -p logs
+rm -rf logs/BOE logs/BORME logs/BOCM
+mkdir -p logs/BOE
+mkdir -p logs/BORME
+mkdir -p logs/BOCM
+
+
+for boe in `seq $anoInicioBOE $anoActual`;
+do
+	echo "[+] node app BOE $boe"
+	node app BOE $boe >> logs/BOE/BOE$boe.log 2>> logs/BOE/BOE$boe.errores.log &
+	sleep 1
+done
+
+
