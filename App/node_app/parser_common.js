5d5176fe9cdcc50564535c1a47b31d5e712e2f5c
Ricardo Benitez Pelletier <rbenitez2012@gmail.com>
2017-11-27 21:16:33 +0100

    atras

--- App/node_app/parser_common.js
+++ App/node_app/parser_common.js
@@ -82,7 +82,11 @@
                                     callback(data, repeat)
                                 })
                             } else {
-                                process.exit(1)
+                                cadsql = "UPDATE lastread set STOP=0 WHERE Type='" + options.type + "' AND anyo = " + data.desde.substr(0, 4)
+                                options.SQL.db.query(cadsql, function (err,record) {
+                                    console.log('proceso obligado a parar')
+                                    process.exit(1)
+                                })
                             }
                         })
 
