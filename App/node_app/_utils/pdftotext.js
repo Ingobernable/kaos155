d06ee920fda759b729d79e0ed4f87e26c6fbf035
Softman <rbenitez2012@gmail.com>
2017-12-09 10:49:38 +0100

    jar

--- App/node_app/_utils/pdftotext.js
+++ App/node_app/_utils/pdftotext.js
@@ -35,6 +35,16 @@ function pdftotext(filename, options) {
         exec(_command, cb);
     }
 
+    pdftotext.prototype.getTikaText = function (cb) {
+        var self = this;
+        var isWin = /^win/.test(process.platform);
+
+        self.add_options([filename]);
+        var _command = (isWin ? path.normalize(path.dirname(__filename) + '/../../bin/win/') : '') + 'curl ' + self.options.additional.join(' ')
+        //console.log(_command)
+        exec(_command, cb);
+    }
+
     pdftotext.prototype.error = function (callback) {
         this.options.error = callback;
         return this;
