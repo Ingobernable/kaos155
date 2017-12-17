3c4ba2a0cc6c9e78586af80b6ddcb8e033a541d6
Softman <rbenitez2012@gmail.com>
2017-12-17 13:04:19 +0100

    FIX DB CREATE

--- App/node_app/sql_common.js
+++ App/node_app/sql_common.js
@@ -56,7 +56,7 @@
             var _this = this
             console.log("\x1b[32m testeando consistencia DB " + db + " \x1b[0m");
             con.query("SHOW Databases LIKE '" + db + "'", function (err, record) {
-                var _command = 'mysql -u' + resp.user + ' -p' + resp.password + ' -h' + resp.host + ' -D' + db + '< ' + app.path.normalize(__dirname + '/../sqlfiles/CREATE_FULL_' + options.Command + '.sql')
+                var _command = 'mysql -u' + resp.user + ' -p' + resp.password + ' -h' + resp.host + ' -D' + db + '< ' + app.path.normalize(__dirname + '/../sqlfiles/' + options.Command.toLowerCase() + '/CREATE_FULL_' + options.Command + '.sql')
 
                 if (record.length == 0) {
                     con.query("CREATE DATABASE IF NOT EXISTS " + db, function (err, result) {
