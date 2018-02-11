91a8fe5244ddab4b7b2a5169c77a3810dc1977dc
Softman65 <rbenitez2012@gmail.com>
2018-02-12 00:18:19 +0100

    .

--- App/app.js
+++ App/app.js
@@ -4,7 +4,8 @@ global.__basedir = __dirname;
 const memwatch = require('memwatch-next');
 
 memwatch.on('leak', (info) => {
-    console.error('Fuga de memoria detectada: \ n', info);
+    console.error('Fuga de memoria detectada:')
+    console.error(info);
 });
 
 const pjson = require('./package.json');
