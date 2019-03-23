199718836d86b5f23463f6ec380d314142c5711b
Ricardo Benitez <rbenitez2012@gmail.com>
2019-03-23 01:35:09 +0100

    .

--- App/node_IA/BORME/IA_Actions.js
+++ App/node_IA/BORME/IA_Actions.js
@@ -4,33 +4,7 @@
 
         tools: {
 
-            relacion: {
-                cadSql: "call Insert_Data_IA_seguimiento(?,?,?)",
-                data: function (argv, iaparams) { return [argv.data._key_relation, argv.data._id_relation, iaparams.min_TRelations] },
-            },
-            movimiento: {
-                cadSql: "CALL insert_Data_IA_movimiento(?,?,?,?,?,?,?,?,?,?,?,?)",
-                data: function (argv) {
-                    return [
-                        argv.data._id_empresa,
-                        argv.data.acto._type,
-
-                        argv.data._fecha.dia,
-                        argv.data._fecha.mes,
-                        argv.data._fecha.anyo,
-                        argv.data._provincia,
-
-                        argv.data.record._Empresa.toString() == '\u0001' ? 1 : 0,
-                        argv.data.record._Financiera.toString() == '\u0001' ? 1 : 0,
-                        argv.data.record._Auditor.toString() == '\u0001' ? 1 : 0,
-                        argv.data.record._Sicav.toString() == '\u0001' ? 1 : 0,
-
-                        argv.data.acto._cargo,
-                        argv.data.acto._value ? (argv.data.acto._type == 'Constitucion' ? argv.data.acto._value.substr(0, 8) : argv.data.acto._value.split('.')[0]) : '',
-
-                    ]
-                }
-            },
+           
             add: {
                 cadSql: {
                     ask: "call sum_KeyTotales(?,?);",
@@ -75,25 +49,6 @@
                     app.writeSync(app, 'ADD->' + argv.data.table + " - " + argv.data.e)
                 }
                 record = null
-            })
-
-        },
-        relacion: function (argv) {
-            //console.log('relacion')
-            //const cadSql = "call Insert_Data_IA_seguimiento(?,?,?)"
-            //const _this = this
-            app.writeSync(app, argv.data.acto._def + ' ' + argv.data.acto._type + ' ' + argv.data.acto._cargo + ' ' + argv.data.acto._value)
-
-            app.IA.SQL.db.query(app._io.functions.BORME.tools.relacion.cadSql, app._io.functions.BORME.tools.relacion.data(argv, app._io._IAparameters), function (err) {
-                if (err)
-                    app.writeSync(app, err)
-            })
-        },
-        movimiento: function (argv) {
-            app.writeSync(app, argv.data.acto._type + ' ' + argv.data.acto._cargo + ' ' + argv.data.acto._value)
-            app.IA.SQL.db.query(app._io.functions.BORME.tools.movimiento.cadSql, app._io.functions.BORME.tools.movimiento.data(argv), function (err) {
-                if (err)
-                    app.writeSync(app, err)
             })
 
         },
